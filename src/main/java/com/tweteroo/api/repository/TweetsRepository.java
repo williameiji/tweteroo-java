57d09075dc57be6e4351b6b37b1c3312050e7669
William <william.fujimori@gmail.com>
2023-01-19 09:49:58 -0300

    feat: add method to find tweets by username

--- src/main/java/com/tweteroo/api/repository/TweetsRepository.java
+++ src/main/java/com/tweteroo/api/repository/TweetsRepository.java
@@ -1,9 +1,12 @@
 package com.tweteroo.api.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.tweteroo.api.model.Tweet;
 
 public interface TweetsRepository extends JpaRepository<Tweet, Long> {
 
+    List<Tweet> findByUsername(String username);
 }
