efb99a18302168057999e1d7fa924369ada31b51
William <william.fujimori@gmail.com>
2023-01-19 08:57:27 -0300

    feat: add post route on tweets

--- src/main/java/com/tweteroo/api/controller/Tweets.java
+++ src/main/java/com/tweteroo/api/controller/Tweets.java
@@ -1,19 +1,25 @@
 package com.tweteroo.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tweteroo.api.dto.TweetsDto;
+import com.tweteroo.api.model.Tweet;
+import com.tweteroo.api.repository.TweetsRepository;
 
 @RestController
 @RequestMapping("/api/tweets")
 public class Tweets {
 
+    @Autowired
+    private TweetsRepository repository;
+
     @PostMapping
     public void NewTweet(@RequestBody TweetsDto req) {
-        System.out.println(req.tweet());
+        repository.save(new Tweet(req));
     }
 
 }
