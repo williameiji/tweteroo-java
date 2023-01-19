cf90c2d9da49c2f00350adc6d1b0c94a0441262b
William <william.fujimori@gmail.com>
2023-01-19 09:51:53 -0300

    feat: add route to get tweets by username

--- src/main/java/com/tweteroo/api/controller/Tweets.java
+++ src/main/java/com/tweteroo/api/controller/Tweets.java
@@ -1,14 +1,21 @@
 package com.tweteroo.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tweteroo.api.dto.TweetsDto;
 import com.tweteroo.api.model.Tweet;
+import com.tweteroo.api.model.User;
 import com.tweteroo.api.repository.TweetsRepository;
+import com.tweteroo.api.repository.UsersRepository;
 
 @RestController
 @RequestMapping("/api/tweets")
@@ -17,9 +24,23 @@ public class Tweets {
     @Autowired
     private TweetsRepository repository;
 
+    @Autowired
+    private UsersRepository usersRepository;
+
     @PostMapping
-    public void NewTweet(@RequestBody TweetsDto req) {
-        repository.save(new Tweet(req));
+    public void newTweet(@RequestBody TweetsDto req) {
+        List<User> user = usersRepository.findByUsername(req.username());
+        repository.save(new Tweet(req, user.get(0).getAvatar()));
+    }
+
+    @GetMapping
+    public List<Tweet> getTweets() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/{username}")
+    public List<Tweet> getTweetsFromUser(@PathVariable String username) {
+        return repository.findByUsername(username);
     }
 
 }
