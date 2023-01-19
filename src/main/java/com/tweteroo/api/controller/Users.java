86dbdf45fb55ed41c8c4207abf89fe032eaf86da
William <william.fujimori@gmail.com>
2023-01-19 08:57:41 -0300

    feat: add post route on users

--- src/main/java/com/tweteroo/api/controller/Users.java
+++ src/main/java/com/tweteroo/api/controller/Users.java
@@ -1,18 +1,24 @@
 package com.tweteroo.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tweteroo.api.dto.UsersDto;
+import com.tweteroo.api.model.User;
+import com.tweteroo.api.repository.UsersRepository;
 
 @RestController
 @RequestMapping("/api/users")
 public class Users {
 
+    @Autowired
+    private UsersRepository repository;
+
     @PostMapping
     public void signUp(@RequestBody UsersDto req) {
-        System.out.println(req.username());
+        repository.save(new User(req));
     }
 }
