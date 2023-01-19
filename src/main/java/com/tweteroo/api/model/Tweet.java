6238405b1c6a0d8f3fa86cbcd2a909c4949b0b61
William <william.fujimori@gmail.com>
2023-01-19 08:54:44 -0300

    feat: add tweet model

---
+++ src/main/java/com/tweteroo/api/model/Tweet.java
@@ -0,0 +1,33 @@
+package com.tweteroo.api.model;
+
+import com.tweteroo.api.dto.TweetsDto;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+public class Tweet {
+
+    public Tweet(TweetsDto data) {
+        this.username = data.username();
+        this.tweet = data.tweet();
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(length = 30, nullable = false)
+    private String username;
+
+    @Column(length = 100, nullable = false)
+    private String tweet;
+
+}
