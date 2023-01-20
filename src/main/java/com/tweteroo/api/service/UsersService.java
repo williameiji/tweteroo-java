b56407404f3a85bec8f51b3436d63176476a1c1b
William <william.fujimori@gmail.com>
2023-01-20 10:05:45 -0300

    feat: add user service

---
+++ src/main/java/com/tweteroo/api/service/UsersService.java
@@ -0,0 +1,19 @@
+package com.tweteroo.api.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tweteroo.api.dto.UsersDto;
+import com.tweteroo.api.model.User;
+import com.tweteroo.api.repository.UsersRepository;
+
+@Service
+public class UsersService {
+
+    @Autowired
+    private UsersRepository repository;
+
+    public void signUp(UsersDto dto) {
+        repository.save(new User(dto));
+    }
+}
