017769a9f1b00f1872eeb3d7f09fe90bcb27bada
William <william.fujimori@gmail.com>
2023-01-20 21:33:51 -0300

    feat: add custom response

--- src/main/java/com/tweteroo/api/controller/Users.java
+++ src/main/java/com/tweteroo/api/controller/Users.java
@@ -1,10 +1,12 @@
 package com.tweteroo.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tweteroo.api.dto.UsersDto;
@@ -19,6 +21,7 @@ public class Users {
     private UsersService service;
 
     @PostMapping("/sign-up")
+    @ResponseStatus(code = HttpStatus.CREATED)
     public void signUp(@RequestBody UsersDto req) {
         service.signUp(req);
     }
