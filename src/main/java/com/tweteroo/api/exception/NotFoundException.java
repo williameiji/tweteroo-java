8a36ee6c24c632dd89c02be2bddeebb6ad6760ec
William <william.fujimori@gmail.com>
2023-01-21 10:30:23 -0300

    feat: add not found exception

---
+++ src/main/java/com/tweteroo/api/exception/NotFoundException.java
@@ -0,0 +1,10 @@
+package com.tweteroo.api.exception;
+
+public class NotFoundException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
