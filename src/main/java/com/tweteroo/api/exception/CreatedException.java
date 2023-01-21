07822ee2a5ebcddb0373fba0920af9ccba88d06a
William <william.fujimori@gmail.com>
2023-01-21 10:29:34 -0300

    feat: add create exception

---
+++ src/main/java/com/tweteroo/api/exception/CreatedException.java
@@ -0,0 +1,10 @@
+package com.tweteroo.api.exception;
+
+public class CreatedException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+    public CreatedException(String message) {
+        super(message);
+    }
+}
